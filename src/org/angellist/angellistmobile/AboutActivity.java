fc984485e295cba3e62bb3b3db112437733d4f4a
Daniel Benedykt <daniel@benedykt.com>
2013-10-21 14:21:17 -0200

    improved about activity

--- src/org/angellist/angellistmobile/AboutActivity.java
+++ src/org/angellist/angellistmobile/AboutActivity.java
@@ -1,8 +1,28 @@
+/*Copyright 2013 Daniel Benedykt
+
+This file is part of AngelList Mobile.
+
+AngelList Mobile is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 2 of the License, or 
+(at your option) any later version.
+
+AngelList Mobile is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with AngelList Mobile. If not, see http://www.gnu.org/licenses/.
+*/
+
 package org.angellist.angellistmobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class AboutActivity extends Activity {
 
@@ -18,6 +38,13 @@ public class AboutActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	
+	/** Called when the user touches the button */
+	public void close(View view) {
+		// Do something in response to button click
+		finish();
 	}
 	
 	
